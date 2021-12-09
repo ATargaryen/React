b73f7ed9320387810f9902c1ac66349f5cd9af77
AmanChaudharyy <Achaudhary@youngman.co.in>
2021-12-09 14:50:19 +0530

    template : single-item

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,17 @@
+import ItemTemplate from '../templates/single-item';
 
 function Body() {
+    
+    const item_property = {
+        name : 'Ladder',
+        src : './assets/images/p2.png',
+        price : '2092',
+    };
     
     return ( <div>     
 
        <h2>Hi, I am a Body!</h2>
-
+       <ItemTemplate  item={item_property}  />
        </div> );
   }
   
---
+++ src/components/templates/single-item.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+function SingleItem(props) {
+
+    const temp_style = {
+      backgroundColor: '#FF0000',
+    };
+
+
+   var [ counter , setCounter] = useState(0);
+
+
+    const setCartCount = () => {
+      setCounter(counter+1);
+    };
+    return ( <div>     
+       <h2>Hi, I am a Body!</h2>
+   
+
+       <div className="item_template w-50" style={ temp_style }>
+       <img src={props.item.src} onClick={ myfunction } alt="item_image" ></img>
+        <h5 className="badge-primary " style={{ fontWeight : "bold", fontSize : 30 , fontFamily : 'serif' }}>{props.item.name}  {props.item.price }  </h5> 
+       </div>
+       
+       <h2 id="item_selected_lbl">{ counter  }</h2> <button className="btn btn-primary" onClick={ setCartCount } >Add To Cart</button> 
+      
+      </div>
+      );
+}
+  
+  export default SingleItem;
+
+
+  function myfunction(){
+    alert('Hello JSX !')
+  }
+
+
+
